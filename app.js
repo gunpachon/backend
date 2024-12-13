666d4d3cca560f0c18e3def126f959afce797c2e
gunn-com <gunkie04@gmail.com>
2024-12-13 11:16:03 +0700

    Add day1

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log('Hello gun');
+console.log('Hello gun');
+console.log('**********');
+console.log('*');
+console.log('*');
+console.log('**********');
+console.log('*');
+console.log('*');
+console.log('*');
+console.log(15);
+console.log(33*65);
+console.log("Hello"+"World");
+console.log(33*65 + " cc");
+console.log(33*65 + " litre");
+// ข้อ 10 : string, number, string, number, number, number, number
+var y = 10 + 5;
+var x = 10 + 5;
+var z = x + y;
+// 12
+// 16
+
+
